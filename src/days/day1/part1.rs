52c7def00bea3826bc4cbbc43e999204d35854b8
rinuya <rinuya@users.noreply.github.com>
2023-02-12 11:03:02 +0100

    Day1 Part1

--- src/days/day1/part1.rs
+++ src/days/day1/part1.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::Read;
+use std::time::Instant;
+
 pub fn run() {
-    println!("Hey from day1 part1")
+    let start = Instant::now();
+
+    let mut string_input = String::new();
+    File::open("src/days/day1/input.txt").unwrap().read_to_string(&mut string_input).unwrap();
+
+    let mut counter = 0;
+    for c in string_input.chars() {
+        match c {
+            '(' => counter += 1,
+            ')' => counter -= 1,
+            _ => continue,
+        }
+    }
+
+    let elapsed = start.elapsed();
+    println!("Day 1 Part 1: The solution '{}' was found in {:.2?}", counter, elapsed);
 }
