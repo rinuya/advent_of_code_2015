94aea22707b7b555dc32e8b2dfd7a299507b6538
rinuya <rinuya@users.noreply.github.com>
2023-02-18 10:19:58 +0100

    Day6 Part1

---
+++ src/days/day6/part1.rs
@@ -0,0 +1,68 @@
+use std::fs::File;
+use std::io::{ self, BufRead };
+use std::time::Instant;
+
+
+pub fn run() {
+    let start = Instant::now();
+
+    let file = File::open("src/days/day6/input.txt").unwrap(); 
+    let lines = io::BufReader::new(file).lines(); 
+    let mut result:u64 = 0;
+    let mut grid = vec![vec![0; 1000]; 1000];
+
+    for line in lines {
+        let _line = line.unwrap();
+        if _line.is_empty() {
+            break;
+        }
+
+        let _line = _line.split(" ").collect::<Vec<_>>();
+        match _line[1] {
+            "on" => {
+                let start = _line[2].split(",").map(|word| word.parse().unwrap()).collect::<Vec<i32>>();
+                let end = _line[4].split(",").map(|word| word.parse().unwrap()).collect::<Vec<i32>>();
+
+                for i in start[0]..end[0]+1 {
+                    for j in start[1]..end[1]+1 {             
+                        grid[i as usize][j as usize] = 1;
+                    }
+                }
+            },
+            "off" => {
+                let start = _line[2].split(",").map(|word| word.parse().unwrap()).collect::<Vec<i32>>();
+                let end = _line[4].split(",").map(|word| word.parse().unwrap()).collect::<Vec<i32>>();
+
+                for i in start[0]..end[0]+1 {
+                    for j in start[1]..end[1]+1 {             
+                        grid[i as usize][j as usize] = 0;
+                    }
+                }
+            },
+            _ => {
+                let start = _line[1].split(",").map(|word| word.parse().unwrap()).collect::<Vec<i32>>();
+                let end = _line[3].split(",").map(|word| word.parse().unwrap()).collect::<Vec<i32>>();
+
+                for i in start[0]..end[0]+1 {
+                    for j in start[1]..end[1]+1 {  
+                        if grid[i as usize][j as usize] == 0 {
+                            grid[i as usize][j as usize] = 1;
+                        } else {
+                            grid[i as usize][j as usize] = 0;
+                        }
+                    }
+                }
+            },
+        }
+    }
+    for i in 0..1000{
+        for j in 0..1000 {
+            if grid[i][j] == 1 {
+                result += 1;
+            }
+        }
+    }
+
+    let elapsed = start.elapsed();
+    println!("Day 6 Part 1: The solution '{:?}' was found in {:.2?}", result, elapsed);
+}
